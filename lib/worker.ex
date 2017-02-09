e9b74290c74a96dc2a82a77777ea8b0dd59b467e
Lance Johnson <lancejjohnson@gmail.com>
2017-02-09 08:49:15 -0500

    Allow worker to be used as process

--- lib/worker.ex
+++ lib/worker.ex
@@ -1,4 +1,33 @@
 defmodule Metex.Worker do
+  @doc """
+  Function that:
+
+  *   sets up a Worker to be used as a process.
+  *   defines the data patterns that the process works with in the `receive`
+      block.
+  *   recurses on itself so that the process can remain active and receive
+      additional messages.
+
+  With this setup the module can be used synchronously within the system
+
+      Metex.Worker.temperature_of("London")
+
+  or asynchronously
+
+      pid = spawn Metex.Worker, :loop, []
+      send pid, {self(), "London"}
+  """
+  def loop do
+    receive do
+      {sender_pid, location} ->
+        send sender_pid, {:ok, temperature_of(location)}
+      _ ->
+        IO.puts "don't know how to process message."
+    end
+
+    loop()
+  end
+
   def temperature_of(location) do
     result = url_for(location) |> HTTPoison.get |> parse_response
     case result do
