13c42bcd5cb5d455bf35a7caafa4b3f7f8b34390
Lance Johnson <lancejjohnson@gmail.com>
2017-03-21 20:37:23 -0400

    Name process and get by name

--- test/metex/worker_test.exs
+++ test/metex/worker_test.exs
@@ -4,25 +4,25 @@ defmodule Metex.WorkerTest do
   alias Metex.Worker
 
   setup do
-    {:ok, worker} = Worker.start_link
-    {:ok, worker: worker}
+    {:ok, _} = Worker.start_link
+    :ok
   end
 
 
-  test "gets the temperature for a location", %{worker: worker} do
-    temp = Worker.get_temperature(worker, "Wake Forest, NC")
+  test "gets the temperature for a location" do
+    temp = Worker.get_temperature("Wake Forest, NC")
     assert temp =~ ~r/\d+°C/
   end
 
-  test "resetting the state of the server", %{worker: worker} do
-     temp = Worker.get_temperature(worker, "Wake Forest, NC")
-     before = Worker.get_stats(worker)
-     :ok = Worker.reset_stats(worker)
+  test "resetting the state of the server" do
+     temp = Worker.get_temperature("Wake Forest, NC")
+     before = Worker.get_stats
+     :ok = Worker.reset_stats
 
-     refute before == Worker.get_stats(worker)
+     refute before == Worker.get_stats
   end
 
-  test "stopping the server", %{worker: worker} do
-    assert :ok == Worker.stop(worker)
+  test "stopping the server" do
+    assert :ok == Worker.stop
   end
 end
